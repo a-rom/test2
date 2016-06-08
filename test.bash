d5d0b300f72a85a443650e227155d824182d30d6
a-rom <a1623at@aiit.ac.jp>
2016-06-09 05:05:15 +0900

    travis CI test again

---
+++ test.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#########
+# TEST1 #
+#########
+echo 10 > $tmp-ans1
+bash ./16745123.bash  100 10 > $tmp-out1 || exit 1
+$tmp-ans1 -eq $tmp-out1 || exit 1
+
+#########
+# TEST2 #
+#########
+echo 6 > $tmp-ans2
+bash ./16745123.bash  12 42 > $tmp-out2 || exit 1
+$tmp-ans2 -eq $tmp-out2 || exit 1
+
+echo 15 > $tmp-ans3
+bash ./16745123.bash  12 42 > $tmp-out3 || exit 1
+$tmp-ans3 -eq $tmp-out3 || exit 1
+
+exit 0
+
