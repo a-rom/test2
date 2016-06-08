920985bd1e63ba1d26d3c8c47747c46c520476d9
a-rom <a1623at@aiit.ac.jp>
2016-06-09 05:19:45 +0900

    travis CI test again

--- test.bash
+++ test.bash
@@ -5,14 +5,14 @@
 #########
 echo 10 > $tmp-ans1
 bash ./16745123.bash  100 10 > $tmp-out1 || exit 1
-$tmp-ans1 -eq $tmp-out1 || exit 1
+$tmp-ans1 == $tmp-out1 || exit 1
 
 #########
 # TEST2 #
 #########
 echo 6 > $tmp-ans2
 bash ./16745123.bash  12 42 > $tmp-out2 || exit 1
-$tmp-ans2 -eq $tmp-out2 || exit 1
+$tmp-ans2 == $tmp-out2 || exit 1
 
 #########
 # TEST3 #
@@ -20,7 +20,7 @@ $tmp-ans2 -eq $tmp-out2 || exit 1
 
 echo 25 > $tmp-ans3
 bash ./16745123.bash  325 200 > $tmp-out3 || exit 1
-$tmp-ans3 -eq $tmp-out3 || exit 1
+$tmp-ans3 == $tmp-out3 || exit 1
 
 exit 0
 
